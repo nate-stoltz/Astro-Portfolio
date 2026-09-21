8fa2f3150cbdc5f2df42f811ab2df7f37c10e5c8
nate-stoltz <nlstoltz@gmail.com>
2026-09-21 07:52:25 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
   site: 'https://nate-stoltz.github.io/Astro-Portfolio',
+  base: '/Astro-Portfolio',
   vite: {
     plugins: [glsl()],
   },
