5ab73cff3cda0a8b5113fd5d3db07ab49479064f
nate-stoltz <nlstoltz@gmail.com>
2026-09-21 09:16:13 -0500

    Update images.js

--- src/lib/webgl/images.js
+++ src/lib/webgl/images.js
@@ -1,16 +1,19 @@
+const base = import.meta.env.BASE_URL.replace(/\/$/, '');
+const img = (path) => `${base}${path}`;
+
 export const images = {
 	landscape: [
-		{ src: '/images/carousel/landscape/xfinity-img-1.jpg', alt: 'Xfinity' },
-		{ src: '/images/carousel/landscape/humana-img-1.jpg', alt: 'Humana' },
-		{ src: '/images/carousel/landscape/lenz-img-1.jpg', alt: 'Lenz' },
-		{ src: '/images/carousel/landscape/tkc-img-1.jpg', alt: 'True Story' },
-		{ src: '/images/carousel/landscape/xfinity-img-2.jpg', alt: 'Xfinity' },
+		{ src: img('/images/carousel/landscape/xfinity-img-1.jpg'), alt: 'Xfinity' },
+		{ src: img('/images/carousel/landscape/humana-img-1.jpg'), alt: 'Humana' },
+		{ src: img('/images/carousel/landscape/lenz-img-1.jpg'), alt: 'Lenz' },
+		{ src: img('/images/carousel/landscape/tkc-img-1.jpg'), alt: 'True Story' },
+		{ src: img('/images/carousel/landscape/xfinity-img-2.jpg'), alt: 'Xfinity' },
 	],
 	portrait: [
-		{ src: '/images/carousel/landscape/xfinity-img-1.jpg', alt: 'Xfinity' },
-		{ src: '/images/carousel/portrait/humana-img-1.jpg', alt: 'Humana' },
-		{ src: '/images/carousel/portrait/lenz-img-1.jpg', alt: 'Lenz' },
-		{ src: '/images/carousel/portrait/tkc-img-1.jpg', alt: 'True Story' },
-		{ src: '/images/carousel/portrait/xfinity-img-2.jpg', alt: 'Xfinity' },
+		{ src: img('/images/carousel/landscape/xfinity-img-1.jpg'), alt: 'Xfinity' },
+		{ src: img('/images/carousel/portrait/humana-img-1.jpg'), alt: 'Humana' },
+		{ src: img('/images/carousel/portrait/lenz-img-1.jpg'), alt: 'Lenz' },
+		{ src: img('/images/carousel/portrait/tkc-img-1.jpg'), alt: 'True Story' },
+		{ src: img('/images/carousel/portrait/xfinity-img-2.jpg'), alt: 'Xfinity' },
 	]
 };
