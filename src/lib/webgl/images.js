dfd3c4f32b37fd91cd8fb4b67a72bdc223670644
nate-stoltz <nlstoltz@gmail.com>
2026-09-21 16:00:19 -0500

    Update images.js

--- src/lib/webgl/images.js
+++ src/lib/webgl/images.js
@@ -1,19 +1,18 @@
-const base = import.meta.env.BASE_URL.replace(/\/$/, '');
-const img = (path) => `${base}${path}`;
+import { withBase } from './utils/url.js';
 
 export const images = {
 	landscape: [
-		{ src: img('/images/carousel/landscape/xfinity-img-1.jpg'), alt: 'Xfinity' },
-		{ src: img('/images/carousel/landscape/humana-img-1.jpg'), alt: 'Humana' },
-		{ src: img('/images/carousel/landscape/lenz-img-1.jpg'), alt: 'Lenz' },
-		{ src: img('/images/carousel/landscape/tkc-img-1.jpg'), alt: 'True Story' },
-		{ src: img('/images/carousel/landscape/xfinity-img-2.jpg'), alt: 'Xfinity' },
+		{ src: withBase('/images/carousel/landscape/xfinity-img-1.jpg'), alt: 'Xfinity' },
+		{ src: withBase('/images/carousel/landscape/humana-img-1.jpg'), alt: 'Humana' },
+		{ src: withBase('/images/carousel/landscape/lenz-img-1.jpg'), alt: 'Lenz' },
+		{ src: withBase('/images/carousel/landscape/tkc-img-1.jpg'), alt: 'True Story' },
+		{ src: withBase('/images/carousel/landscape/xfinity-img-2.jpg'), alt: 'Xfinity' },
 	],
 	portrait: [
-		{ src: img('/images/carousel/landscape/xfinity-img-1.jpg'), alt: 'Xfinity' },
-		{ src: img('/images/carousel/portrait/humana-img-1.jpg'), alt: 'Humana' },
-		{ src: img('/images/carousel/portrait/lenz-img-1.jpg'), alt: 'Lenz' },
-		{ src: img('/images/carousel/portrait/tkc-img-1.jpg'), alt: 'True Story' },
-		{ src: img('/images/carousel/portrait/xfinity-img-2.jpg'), alt: 'Xfinity' },
+		{ src: withBase('/images/carousel/portrait/xfinity-img-1.jpg'), alt: 'Xfinity' },
+		{ src: withBase('/images/carousel/portrait/humana-img-1.jpg'), alt: 'Humana' },
+		{ src: withBase('/images/carousel/portrait/lenz-img-1.jpg'), alt: 'Lenz' },
+		{ src: withBase('/images/carousel/portrait/tkc-img-1.jpg'), alt: 'True Story' },
+		{ src: withBase('/images/carousel/portrait/xfinity-img-2.jpg'), alt: 'Xfinity' },
 	]
 };
